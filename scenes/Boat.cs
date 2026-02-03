a7c073190121cacb36c3b5264a9d77a5c9038989
zanebennion <zanebennion@gmail.com>
2026-02-03 12:27:27 -0700

    clean slate

--- scenes/Boat.cs
+++ scenes/Boat.cs
@@ -6,11 +6,6 @@ using Waterways;
 public partial class Boat : RigidBody3D
 {
     [Export] public RiverFloatSystem River { get; set;}
-    [Export] public float WaterDensity = 25.0f;
-    [Export] public float FlowForceMultiplier = 10.0f;
-    [Export] public float WaterDrag = 2.0f;
-    [Export] public float FlowSpeed = 5.0f;
-    
     private Node3D _probeContainer;
 
     public override void _Ready()
@@ -22,13 +17,7 @@ public partial class Boat : RigidBody3D
     {
         foreach (Marker3D probe in _probeContainer.GetChildren().OfType<Marker3D>())
         {
-            Vector3 pos = probe.GlobalPosition;
-
-            Vector3 flowDirection = River.GetWaterFlowDirection(pos);
-            Vector3 riverRight = flowDirection.Cross(Vector3.Up).Normalized();
-            Vector3 riverNormal = riverRight.Cross(flowDirection).Normalized();
             
-            ApplyForce(riverNormal * 2, probe.GlobalPosition - GlobalPosition);
         }
     }
 }
