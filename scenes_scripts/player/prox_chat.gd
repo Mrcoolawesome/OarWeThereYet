03149422b72cc5b7e932fc895bd149ed05925ae6
zanebennion <zanebennion@gmail.com>
2026-02-23 14:12:11 -0700

    prox chat script

---
+++ scenes_scripts/player/prox_chat.gd
@@ -0,0 +1,35 @@
+extends Node
+
+var current_sample_rate: int = 48000
+@export var use_recommended_sample_rate: bool = false
+
+func _ready() -> void:
+	# Start recording when player loads in, could make an option for push to talk later
+	Steam.startVoiceRecording()
+
+
+# Get voice data, compress it, and send it
+func _process(delta: float) -> void:
+	var voice_data = Steam.getVoice()
+
+	if voice_data['result'] == Steam.VOICE_RESULT_OK and voice_data['written'] > 0:
+		send_voice.rpc(voice_data['buffer'])
+
+# Decompress received voice data
+func process_voice(buffer: PackedByteArray):
+	get_sample_rate()
+
+	var decompressed_voice: Dictionary = Steam.decompressVoice(buffer, current_sample_rate)
+
+# Get steam's recommended sample rate if you want
+func get_sample_rate():
+	if use_recommended_sample_rate:
+		current_sample_rate = Steam.getVoiceOptimalSampleRate()
+	else:
+		current_sample_rate = 48000
+
+# Send voice data over the internet
+@rpc("any_peer", "call_remote", "unreliable", 1)
+func send_voice(buffer: PackedByteArray):
+	var sender_id = multiplayer.get_remote_sender_id()
+	process_voice(buffer)
