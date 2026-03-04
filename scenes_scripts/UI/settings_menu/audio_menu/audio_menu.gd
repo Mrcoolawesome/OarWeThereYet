fa26c27cc44db75e2372d091ba1eb4b4e08ab33e
Devin Schutz <schutzdevin@gmail.com>
2026-03-03 20:55:34 -0700

    I broke the volume slider and fixed it]

--- scenes_scripts/UI/settings_menu/audio_menu/audio_menu.gd
+++ scenes_scripts/UI/settings_menu/audio_menu/audio_menu.gd
@@ -1,5 +1,14 @@
 extends Control
 
+@onready var vc_volume_slider = $MarginContainer/VoicechatVolumeSlider
+
+func _ready() -> void:
+	# set the slider tag immedately 
+	vc_volume_slider.number_tag.text = str(int(vc_volume_slider.StartingValue))
+
 func _on_voice_chat_volume_slider_value_changed(value: float) -> void:
 	# set the volume via linearly 
 	AudioServer.set_bus_volume_linear(AudioServer.get_bus_index("Voice Chat"), value);
+
+	# set the slider's number tag value
+	vc_volume_slider.number_tag.text = str(int(value))
