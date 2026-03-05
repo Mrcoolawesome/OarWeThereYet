53eb8497208bf3ca8724e05e3c3b1e7516e92a00
Devin Schutz <schutzdevin@gmail.com>
2026-03-05 11:10:15 -0700

    maybe fixed the volume

--- scenes_scripts/UI/settings_menu/audio_menu/audio_menu.gd
+++ scenes_scripts/UI/settings_menu/audio_menu/audio_menu.gd
@@ -4,11 +4,11 @@ extends Control
 
 func _ready() -> void:
 	# set the slider tag immedately 
-	vc_volume_slider.number_tag.text = str(int(vc_volume_slider.StartingValue))
+	vc_volume_slider.number_tag.text = str(int(vc_volume_slider.StartingValue * 100))
 
 func _on_voice_chat_volume_slider_value_changed(value: float) -> void:
 	# set the volume via linearly 
 	AudioServer.set_bus_volume_linear(AudioServer.get_bus_index("Voice Chat"), value);
 
 	# set the slider's number tag value
-	vc_volume_slider.number_tag.text = str(int(value))
+	vc_volume_slider.number_tag.text = str(int(value * 100))
