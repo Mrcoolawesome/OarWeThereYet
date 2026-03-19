1f05eedd7a45c674a645f035a14dff9010dfcdc1
zanebennion <zanebennion@gmail.com>
2026-03-19 11:10:37 -0600

    HOLY FUCK GDSCRIPT INDEDNTSDATION VSCSCODEF FUCK

--- scenes_scripts/multiplayer/networks/enet_network.gd
+++ scenes_scripts/multiplayer/networks/enet_network.gd
@@ -13,92 +13,92 @@ var level_name = "DemoLevel"
 
 # become host for ENet server
 func become_host():
-	# create the server and set the peer of this instance to be the server peer
-	multiplayer_peer.create_server(SERVER_PORT)
-	multiplayer.multiplayer_peer = multiplayer_peer
+    # create the server and set the peer of this instance to be the server peer
+    multiplayer_peer.create_server(SERVER_PORT)
+    multiplayer.multiplayer_peer = multiplayer_peer
 
-	# connect the the callback functions to run when a signal is sent for when a player disconnects or connects
-	multiplayer.peer_connected.connect(_add_player_to_game)
-	multiplayer.peer_disconnected.connect(_remove_player)
+    # connect the the callback functions to run when a signal is sent for when a player disconnects or connects
+    multiplayer.peer_connected.connect(_add_player_to_game)
+    multiplayer.peer_disconnected.connect(_remove_player)
 
-	# add the level first
-	_add_level()
+    # add the level first
+    _add_level()
 
-	# add the server host's player and set its id to 1
-	_add_player_to_game(1)
+    # add the server host's player and set its id to 1
+    _add_player_to_game(1)
 
 # this is the function that's called by the multiplayer manager
 func join_as_client(_lobby_id):
-	# set the peer of the current instance to be a peer
-	multiplayer_peer.create_client(SERVER_IP, SERVER_PORT)
-	multiplayer.multiplayer_peer = multiplayer_peer
+    # set the peer of the current instance to be a peer
+    multiplayer_peer.create_client(SERVER_IP, SERVER_PORT)
+    multiplayer.multiplayer_peer = multiplayer_peer
 
 func _add_level():
-	# only add the level if the current instance is the server
-	if multiplayer.is_server():
-		# load the level
-		var test_level = level_scene.instantiate()
-		test_level.set("SaveSlot", GlobalVariables.save_slot)
-		level_container.add_child(test_level)
+    # only add the level if the current instance is the server
+    if multiplayer.is_server():
+        # load the level
+        var test_level = level_scene.instantiate()
+        test_level.set("SaveSlot", GlobalVariables.save_slot)
+        level_container.add_child(test_level)
 
 func _add_player_to_game(id: int):
-	# this is where we have to add them properly to the level	
-	# the Level container should always be there, just need to check if it has a level actually loaded (as a child) in it
-	if level_container.get_child_count() > 0:
-		# Get the actual map node (the first child of the Level container)
-		var current_map = level_container.get_child(0)
+    # this is where we have to add them properly to the level	
+    # the Level container should always be there, just need to check if it has a level actually loaded (as a child) in it
+    if level_container.get_child_count() > 0:
+        # Get the actual map node (the first child of the Level container)
+        var current_map = level_container.get_child(0)
 
-		# Ensure dynamic map setup (boat spawn, references, etc.) is done before adding players.
-		if current_map.has_method("get") and current_map.has_signal("BoatReady"):
-			if !current_map.get("IsBoatReady"):
-				await current_map.BoatReady
+        # Ensure dynamic map setup (boat spawn, references, etc.) is done before adding players.
+        if current_map.has_method("get") and current_map.has_signal("BoatReady"):
+            if !current_map.get("IsBoatReady"):
+                await current_map.BoatReady
 
-		# instantiate a new player object
-		var player = player_scene.instantiate()
-		player.name = str(id) # set the name of the player to be their client id
-		
-		# Add the player as a child of the LOADED MAP
-		current_map.add_child(player, true) # that second boolean is important because it keeps the name of the player to be the one that we set for it
-		
-		# assign the camera to the player for the terrain3d addon
-		rpc_id(id, "_assign_camera", id)
+        # instantiate a new player object
+        var player = player_scene.instantiate()
+        player.name = str(id) # set the name of the player to be their client id
+        
+        # Add the player as a child of the LOADED MAP
+        current_map.add_child(player, true) # that second boolean is important because it keeps the name of the player to be the one that we set for it
+        
+        # assign the camera to the player for the terrain3d addon
+        rpc_id(id, "_assign_camera", id)
 
-	else:
-		print("Error: Cannot spawn player. No map is currently loaded in the Level node.")
+    else:
+        print("Error: Cannot spawn player. No map is currently loaded in the Level node.")
 
 @rpc("authority", "reliable")
 func _assign_camera(id: int) -> void:
-	var current_map = level_container.get_child(0)
-	# Check if the player we just spawned is OUR local player
-	if id == multiplayer.get_unique_id():
-		# Grab the Terrain3D node from the map
-		var terrain = current_map.get_node_or_null("Terrain3D")
+    var current_map = level_container.get_child(0)
+    # Check if the player we just spawned is OUR local player
+    if id == multiplayer.get_unique_id():
+        # Grab the Terrain3D node from the map
+        var terrain = current_map.get_node_or_null("Terrain3D")
 
-		# Grab the Camera3D from the newly spawned player
-		var camera_path = str(id) + "/Head/CameraContainer/Camera3D"
-		var local_camera = current_map.get_node_or_null(camera_path)
+        # Grab the Camera3D from the newly spawned player
+        var camera_path = str(id) + "/Head/CameraContainer/Camera3D"
+        var local_camera = current_map.get_node_or_null(camera_path)
 
-		if terrain and local_camera:
-			terrain.set_camera(local_camera)
-			print("Successfully linked local camera to Terrain3D!")
-		else:
-			print("Could not link camera. Terrain or Camera node missing.")
+        if terrain and local_camera:
+            terrain.set_camera(local_camera)
+            print("Successfully linked local camera to Terrain3D!")
+        else:
+            print("Could not link camera. Terrain or Camera node missing.")
 
 '''
 	find the player we're looking to remove, and remove their instance.
 '''
 func _remove_player(id : int):		
-	# recursively looks for the player
-	var active_level = level_container.get_node_or_null(level_name)
-	var player_node = active_level.get_node_or_null(str(id))
-	
-	if player_node:
-		# Player drops item if they're holding it
-		var arm_node = player_node.get_node_or_null("Head/ArmNode")
-		if arm_node:
-			arm_node.DropItem(Vector3.ZERO)
+    # recursively looks for the player
+    var active_level = level_container.get_node_or_null(level_name)
+    var player_node = active_level.get_node_or_null(str(id))
+    
+    if player_node:
+        # Player drops item if they're holding it
+        var arm_node = player_node.get_node_or_null("Head/ArmNode")
+        if arm_node:
+            arm_node.DropItem(Vector3.ZERO)
 
-		# Free player
-		player_node.queue_free()
-	else:
-		print("Could not find player with ID: ", id)
+        # Free player
+        player_node.queue_free()
+    else:
+        print("Could not find player with ID: ", id)
