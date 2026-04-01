d59e821cf115afc5a6313d5eae2180616860f481
Zane Bennion <zanebennion@gmail.com>
2026-03-31 19:48:17 -0600

    patching holes

--- scenes_scripts/boat/holes/Hole.cs
+++ scenes_scripts/boat/holes/Hole.cs
@@ -1,15 +1,14 @@
 using Godot;
 using System;
 
-public partial class Hole : Node3D
+public partial class Hole : StaticBody3D
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+	[Rpc(MultiplayerApi.RpcMode.AnyPeer, CallLocal = true)]
+	public void RequestPatch()
 	{
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
+		if (Multiplayer.IsServer())
+		{
+			QueueFree();
+		}
 	}
 }
--- scenes_scripts/inventory/items/itemResources/patch/Patch.cs
+++ scenes_scripts/inventory/items/itemResources/patch/Patch.cs
@@ -4,8 +4,13 @@ using System;
 [GlobalClass]
 public partial class Patch : ItemAction
 {
-  public override void Use(Player _player, ArmNode _arm)
+  public override void Use(Player player, ArmNode arm)
   {
+    GodotObject target = player.GetRaycastObject();
 
+    if (target is Hole hole)
+    {
+      hole.Rpc(nameof(Hole.RequestPatch));
+    }
   }
 }
