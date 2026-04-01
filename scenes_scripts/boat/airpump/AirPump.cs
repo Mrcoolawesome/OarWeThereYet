d3358fafb412b656862f25b8922d1ae39c029c94
Zane Bennion <zanebennion@gmail.com>
2026-04-01 16:40:39 -0600

    pumping animation

--- scenes_scripts/boat/airpump/AirPump.cs
+++ scenes_scripts/boat/airpump/AirPump.cs
@@ -3,51 +3,62 @@ using System;
 
 public partial class AirPump : StaticBody3D, Interactable
 {
-	[Export] public string PromptMessage { get; set; } = "Pump Air";
-  [Export] public int HealRate = 1;
-	public string PromptInput { get; set; } = "action_key";
+    [Export] public string PromptMessage { get; set; } = "Pump Air";
+    [Export] public int HealRate = 3;
+    public string PromptInput { get; set; } = "action_key";
 
-  private bool _isPumping = false;
-  private double _timer = 0.0;
-  private Boat _boat;
+    private bool _isPumping = false;
+    private double _timer = 0.0;
+    private Boat _boat;
+    private AnimationPlayer _animationPlayer;
 
-  public override void _Ready()
-  {
-      _boat = GetParent<Boat>();
-  }
+    public override void _Ready()
+    {
+        _boat = GetParent<Boat>();
+        _animationPlayer = GetNode("airPump").GetNode<AnimationPlayer>("AnimationPlayer");
+    }
 
-  public override void _Process(double delta)
-  {
-      if (!Multiplayer.IsServer()) return;
-      if (!_isPumping) return;
+    public override void _Process(double delta)
+    {
+        if (!Multiplayer.IsServer()) return;
+        if (!_isPumping) return;
 
-      if ((_timer += delta) >= 1.0)
-      {
-          _timer -= 1.0;
-          _boat.HealthComponent.UpdateHealth(HealRate);
-      }
-  }
+        if ((_timer += delta) >= 2.0)
+        {
+            _timer -= 2.0;
+            _boat.HealthComponent.UpdateHealth(HealRate);
+        }
+    }
 
-	public void Interact(Player player)
-	{
-	}
+    public void Interact(Player player)
+    {
+    }
 
-  public void StartInteract(Player player)
-  {
-      RpcId(1, nameof(SetPumping), true);
-  }
+    public void StartInteract(Player player)
+    {
+        Rpc(nameof(SetPumping), true);
+    }
 
-  public void StopInteract(Player player)
-  {
-      RpcId(1, nameof(SetPumping), false);
-  }
+    public void StopInteract(Player player)
+    {
+        Rpc(nameof(SetPumping), false);
+    }
 
-  [Rpc(MultiplayerApi.RpcMode.AnyPeer, CallLocal = true)]
-  private void SetPumping(bool pumping)
-  {
-      if (Multiplayer.IsServer())
-      {
-          _isPumping = pumping;
-      }
-  }
+    [Rpc(MultiplayerApi.RpcMode.AnyPeer, CallLocal = true)]
+    private void SetPumping(bool pumping)
+    {
+        _isPumping = pumping;
+
+        if (pumping)
+        {
+            if (_animationPlayer.HasAnimation("pumping"))
+            {
+                _animationPlayer.Play("pumping");
+            }
+        }
+        else
+        {
+            _animationPlayer.Stop();
+        }
+    }
 }
