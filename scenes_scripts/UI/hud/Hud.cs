2bbfbd44f3817eaa9bb80a82b1fed4e04da68872
Devin Schutz <schutzdevin@gmail.com>
2026-04-13 14:29:24 -0600

    hopefully this fixes the hud breaking

--- scenes_scripts/UI/hud/Hud.cs
+++ scenes_scripts/UI/hud/Hud.cs
@@ -9,8 +9,7 @@ public partial class Hud : CanvasLayer
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		// Grab the custom GDScript health bar node
-		_boatHealthBar = GetNode<Control>("BoatHealthBar");
+		TryResolveBoatHealthBar();
 
 		// Subscribe to the boat health update
 		GlobalSignalServer.Instance.UpdateBoatHealth += UpdateBoatHealthUi;
@@ -19,12 +18,36 @@ public partial class Hud : CanvasLayer
 		UpdateBoatHealthUi(GlobalSignalServer.Instance.Health);
 	}
 
+	public override void _ExitTree()
+	{
+		if (GlobalSignalServer.Instance != null)
+		{
+			GlobalSignalServer.Instance.UpdateBoatHealth -= UpdateBoatHealthUi;
+		}
+	}
+
 	// Updates the boat health ui
 	private void UpdateBoatHealthUi(int newHealth)
 	{
+		if (!TryResolveBoatHealthBar())
+		{
+			return;
+		}
+
 		// Call the GDScript function we just made.
 		// Argument 1: The target health (cast to float)
 		// Argument 2: The duration of the animation in seconds (e.g., 0.5f)
 		_boatHealthBar.Call("set_health_smoothly", (float)newHealth, 0.5f);
+	}
+
+	private bool TryResolveBoatHealthBar()
+	{
+		if (GodotObject.IsInstanceValid(_boatHealthBar))
+		{
+			return true;
+		}
+
+		_boatHealthBar = GetNodeOrNull<Control>("BoatHealthBar");
+		return GodotObject.IsInstanceValid(_boatHealthBar);
 	}
 }
