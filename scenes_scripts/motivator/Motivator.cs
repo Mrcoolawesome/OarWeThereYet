72f074a1ba3a24a14352b4c96cf9d8a064a0ac7f
Zane Bennion <zanebennion@gmail.com>
2026-04-14 11:59:59 -0600

    fish is moving

--- scenes_scripts/motivator/Motivator.cs
+++ scenes_scripts/motivator/Motivator.cs
@@ -7,18 +7,51 @@ public partial class Motivator : Area3D
 	[Export] public float Speed = 1.0f;
 	[Export] public RiverManager RiverNode;
 
+	private float _currentOffset = 0f;
+	private bool _isMoving = false;
 
-	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		GlobalSignalServer.Instance.StartMotivator += //Start logic function
-		GlobalSignalServer.Instance.StartMotivator += //Stop logic function
+		GlobalSignalServer.Instance.StartMotivator += OnStartMotivator;
+		GlobalSignalServer.Instance.StopMotivator += OnStopMotivator;
+
+		if (RiverNode != null && RiverNode.Curve != null)
+		{
+			Vector3 localPos = RiverNode.ToLocal(GlobalPosition);
+			_currentOffset = RiverNode.Curve.GetClosestOffset(localPos);
+		}
+
+		BodyEntered += OnBodyEntered;
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	public override void _PhysicsProcess(double delta)
 	{
-		// Reset level if collide with something on boat layer (layer 2)
-		GlobalSignalServer.Instance.EmitSignal(nameof(GlobalSignalServer.ResetLevel));
+		if (_isMoving && RiverNode != null && RiverNode.Curve != null)
+		{
+			_currentOffset += Speed * (float)delta;
+			
+			Vector3 localPoint = RiverNode.Curve.SampleBaked(_currentOffset);
+			GlobalPosition = RiverNode.ToGlobal(localPoint);
+		}
+	}
+
+	private void OnStartMotivator()
+	{
+		_isMoving = true;
+	}
+
+	private void OnStopMotivator()
+	{
+		_isMoving = false;
+	}
+
+	private void OnBodyEntered(Node3D body)
+	{
+		if (!Multiplayer.IsServer()) return;
+
+		if (body.Name == "Boat")
+		{
+			GlobalSignalServer.Instance.EmitSignal(nameof(GlobalSignalServer.ResetLevel));
+		}
 	}
 }
