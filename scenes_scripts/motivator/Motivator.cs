5dc2904d9d148661b68aac0c59cf5ff24b224b3f
Zane Bennion <zanebennion@gmail.com>
2026-04-14 12:11:21 -0600

    fish animates and looks down river

--- scenes_scripts/motivator/Motivator.cs
+++ scenes_scripts/motivator/Motivator.cs
@@ -8,7 +8,8 @@ public partial class Motivator : Area3D
 	[Export] public RiverManager RiverNode;
 
 	private float _currentOffset = 0f;
-	private bool _isMoving = false;
+	private bool _isMoving = true;
+	private AnimationPlayer _animationPlayer;
 
 	public override void _Ready()
 	{
@@ -22,6 +23,10 @@ public partial class Motivator : Area3D
 		}
 
 		BodyEntered += OnBodyEntered;
+
+		// Assuming the fish.blend instance contains the AnimationPlayer
+		_animationPlayer = GetNodeOrNull<AnimationPlayer>("fish/AnimationPlayer");
+		UpdateAnimationState();
 	}
 
 	public override void _PhysicsProcess(double delta)
@@ -30,19 +35,52 @@ public partial class Motivator : Area3D
 		{
 			_currentOffset += Speed * (float)delta;
 			
+			// 1. Update Position
 			Vector3 localPoint = RiverNode.Curve.SampleBaked(_currentOffset);
 			GlobalPosition = RiverNode.ToGlobal(localPoint);
+
+			// 2. Update Rotation (Look down the river)
+			// Get the tangent (forward direction) at the current offset
+			// SampleBakedWithRotation or just sampling a point slightly ahead works too
+			Vector3 nextLocalPoint = RiverNode.Curve.SampleBaked(_currentOffset + 0.1f);
+			Vector3 nextGlobalPoint = RiverNode.ToGlobal(nextLocalPoint);
+			
+			// LookAt is a simple way to align the -Z axis with the target. 
+			// We use GlobalPosition and nextGlobalPoint.
+			if (GlobalPosition.DistanceSquaredTo(nextGlobalPoint) > 0.0001f)
+			{
+				LookAt(nextGlobalPoint, Vector3.Up);
+			}
 		}
 	}
 
 	private void OnStartMotivator()
 	{
 		_isMoving = true;
+		UpdateAnimationState();
 	}
 
 	private void OnStopMotivator()
 	{
 		_isMoving = false;
+		UpdateAnimationState();
+	}
+
+	private void UpdateAnimationState()
+	{
+		if (_animationPlayer == null) return;
+
+		if (_isMoving)
+		{
+			if (_animationPlayer.HasAnimation("Swim"))
+			{
+				_animationPlayer.Play("Swim");
+			}
+		}
+		else
+		{
+			_animationPlayer.Stop();
+		}
 	}
 
 	private void OnBodyEntered(Node3D body)
