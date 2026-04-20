966009a87d7025fc8d23b108424d2e49df313888
Zane Bennion <zanebennion@gmail.com>
2026-04-20 13:43:06 -0600

    reverted manual handshake for rpc calls

--- scenes_scripts/boat/holes/Hole.cs
+++ scenes_scripts/boat/holes/Hole.cs
@@ -3,24 +3,9 @@ using System;
 
 public partial class Hole : StaticBody3D
 {
-	private bool _hasPendingPatchIntent = false;
-
-	[Rpc(MultiplayerApi.RpcMode.AnyPeer, CallLocal = false)]
-	public void RequestPatchConfirmation()
-	{
-		if (!Multiplayer.IsServer()) return;
-		Rpc(nameof(ConfirmPatchRemoval));
-	}
-
 	[Rpc(MultiplayerApi.RpcMode.AnyPeer, CallLocal = true)]
-	private void ConfirmPatchRemoval()
+	public void RequestPatch()
 	{
-		if (_hasPendingPatchIntent)
-		{
-			_hasPendingPatchIntent = false;
-		}
-
-
 		if (Multiplayer.IsServer())
 		{
 			QueueFree();
--- scenes_scripts/inventory/items/itemResources/patch/Patch.cs
+++ scenes_scripts/inventory/items/itemResources/patch/Patch.cs
@@ -10,7 +10,7 @@ public partial class Patch : ItemAction
 
     if (target is Hole hole)
     {
-      player.RequestPatch(hole);
+      hole.Rpc(nameof(Hole.RequestPatch));
 
       // Consume one patch
       int newAmount = arm.Item.Amount - 1;
