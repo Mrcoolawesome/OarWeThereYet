94b038e5bbc5a4b3e5c3d75d4e1e36ded22b6015
Zane Bennion <zanebennion@gmail.com>
2026-04-21 14:26:47 -0600

    holes can only consume one patch

--- scenes_scripts/boat/holes/Hole.cs
+++ scenes_scripts/boat/holes/Hole.cs
@@ -3,11 +3,16 @@ using System;
 
 public partial class Hole : StaticBody3D
 {
+	private bool _isPatched = false;
+
 	[Rpc(MultiplayerApi.RpcMode.AnyPeer, CallLocal = true)]
 	public void RequestPatch(string armPath)
 	{
 		if (Multiplayer.IsServer())
 		{
+			if (_isPatched) return;
+			_isPatched = true;
+
 			ArmNode arm = GetNodeOrNull<ArmNode>(armPath);
 			if (arm != null && arm.Item != null && arm.Item.Data != null)
 			{
